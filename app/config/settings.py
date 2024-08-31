7ca6785c04e09c6b30e58297ca58553a44066f59
kamileg <kamil.kalicki.dev@gmail.com>
2024-08-31 17:00:10 +0200

    added user model

--- app/config/settings.py
+++ app/config/settings.py
@@ -13,6 +13,8 @@ import os
 from pathlib import Path
 
 import os
+
+from django.conf.global_settings import AUTH_USER_MODEL
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -149,3 +151,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'core.User'
